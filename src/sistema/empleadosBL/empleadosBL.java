77f21680377316f5f49ed221cc6557d1679b01e6
feder <feder@DESKTOP-EJK9QDA.fibertel.com.ar>
2022-03-12 16:27:37 -0300

    Primer Commit

---
+++ src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+/**
+ *
+ * @author feder
+ */
+public class empleadosBL {
+    
+    int ID;
+    String nombre;
+    String correo;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
